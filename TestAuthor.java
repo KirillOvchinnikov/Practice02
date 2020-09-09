7a918a0e991ab59d5a169b9194433fd6bc1f57ff
KirillOvchinnikov <kirill.ovchinnikov.1968@mail.ru>
2020-09-09 14:49:28 +0300

    commit

---
+++ TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.Practice02;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Kirill", "kirill@gmail.com", 'M');
+        author.setEmail("kirill56@gmail.com");
+        System.out.println(author);
+    }
+}
